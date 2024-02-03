c47e70d2fb6e0a20e2740670e018ed7cdbff0d95
Divya <divyagarg4747@gmail.com>
2024-02-03 15:31:17 +0530

    changed request model

---
+++ models/VerificationRequest.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const VerificationRequestSchema = new mongoose.Schema({
+  requesterId: { type: String, required: true },
+  receiverId: { type: String, required: true },
+  details: { type: String, required: true },
+  status: { type: String, enum: ['Pending', 'Accepted', 'Rejected'], default: 'Pending' },
+  response: { type: String, default: 'False' },
+});
+
+ 
+
+module.exports = mongoose.models.VerificationRequest || mongoose.model('VerificationRequest', VerificationRequestSchema);
