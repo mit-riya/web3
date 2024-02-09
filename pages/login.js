3abad70a3d096faf5ac48485b29a044de53d25e7
Himanshi <ghimanshi941@gmail.com>
2024-02-09 17:04:06 +0530

    login page styled

--- pages/login.js
+++ pages/login.js
@@ -3,7 +3,7 @@ import { useRouter } from 'next/router';
 import { useContext } from 'react';
 import { UserContext } from './context/userContext';
 import SetUser from './context/setUser';
-
+import styles from './../styles/login.module.css'
 const Login = () => {
     const { account, logout } = useContext(UserContext);
     const router = useRouter();
@@ -21,15 +21,15 @@ const Login = () => {
     };
 
     return (
-        <div>
-            <h1>Login Page</h1>
+        <div class={styles.container}>
+            <h1 className={styles.heading1}>LOGIN PAGE</h1>
             {account ? (
                 <div>
-                    <p>Connected Account: {account}</p>
-                    <p>Account Number: {account}</p>
-                    <button onClick={goToMyIdentities}>My Identities</button>
-                    <button onClick={goToOthersIdentities}>Others' Identities</button>
-                    <button onClick={logoutMetamask}>Logout</button>
+                    <p className={styles.text}>Connected Account: {account}</p>
+                    <p className={styles.text}>Account Number: {account}</p>
+                    <button className={styles.buttonType1} onClick={goToMyIdentities}>My Identities</button>
+                    <button className={styles.buttonType1} onClick={goToOthersIdentities}>Others' Identities</button>
+                    <button className={styles.buttonType1} onClick={logoutMetamask}>Logout</button>
                 </div>
             ) : (
                 <SetUser/>
