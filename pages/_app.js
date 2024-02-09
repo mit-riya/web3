97d28d1fc57933826499e9e2d82e41e2c4bd0962
ShallyKandoi <95773712+ShallyKandoi@users.noreply.github.com>
2024-02-09 15:15:46 +0530

    modal

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import '@/styles/globals.css';
+import Modal from 'react-modal';
 import UserContextProvider from './context/userContext';
+
+Modal.setAppElement('#__next');
 
 export default function App({ Component, pageProps }) {
   return (
