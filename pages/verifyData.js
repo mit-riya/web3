77b085827f6ae14af4d0a1e709281e62a49d9f9e
ShallyKandoi <95773712+ShallyKandoi@users.noreply.github.com>
2024-02-09 16:41:26 +0530

    veriData page updated

--- pages/verifyData.js
+++ pages/verifyData.js
@@ -1,6 +1,9 @@
 import useSWR, {mutate} from 'swr';
 import { useEffect, useState, useRef } from 'react';
 import MultiSelectDropdown from '../components/dropdown';
+import Modal from 'react-modal';
+import { set } from 'mongoose';
+import ContractDataModal from '../components/VerificationStatus';
 
 const fetcher = async (url) => {
   const response = await fetch(url);
@@ -11,25 +14,56 @@ const fetcher = async (url) => {
 const userId = "req10";
 
 const fixedIdentities = [
-  "Aadhar",
-  "Pan Card",
-  "Voter ID",
-  "Driving License",
-  "Passport",
-  "Birth Certificate",
-  "10th Certificate",
-  "12th Certificate"
+  "10th Board Certificate",
+    "12th Board Certificate",
+    "Voter ID",
+    "Passport",
+    "Bachelors Degree - Tech",
+    "Bachelors Degree - Science",
+    "Bachelors Degree - Design",
+    "Masters Degree - Tech",
+    "Masters Degree - Science",
+    "Masters Degree - Design",
+    "Phd Degree",
+    "Courses - Blockchain",
+    "Courses - DSA",
+    "Courses - Probability",
+    "Courses - Machine Learning",
+    "Courses - Product Design",
+    "Work Experience - Software developer",
+    "Work Experience - Data scientist",
+    "Work Experience - Product Manager",
+    "Work Experience - Team lead",
+    "Work Experience - Consultant",
+    "Work Experience - Internship",
+    "Achievements - Gsoc Contributor",
+    "Achievements - Inter IIT Participant",
+    "Achievements - KVPY",
+    "Achievements - NTSE",
+    "Achievements - IMS"
 ];
 
-const YourComponent = () => {
-const [receiverId, setReceiverId] = useState('');
-const [requesterId, setRequesterId] = useState('');
-  // const [details, setDetails] = useState('');
+const VerifyDataPage = () => {
+  const [receiverId, setReceiverId] = useState('');
+  const [requesterId, setRequesterId] = useState('');
+  const [CIDmodalOpen, setCIDModalOpen] = useState(false);
+  const [contractModalOpen, setContractModalOpen] = useState(false);
+  const [contractResultModalOpen, setContractResultModalOpen] = useState(false);
   const [selectedIdentities, setSelectedIdentities] = useState('');
   const url = 'http://localhost:3000/api/fetchAll';
   const { data, error } = useSWR(url, fetcher);
   const [filteredRequests, setFilteredRequests] = useState([]);
   const prevFilteredRequestsLength = useRef(0);
+
+  const handleDirectRequest = () => {
+    setSelectedIdentities('');
+    setContractModalOpen(true);
+  };
+
+  const handleAskCID = () => {
+    setSelectedIdentities('');
+    setCIDModalOpen(true);
+  }
 
   useEffect(() => {
     if (data) {
@@ -52,7 +86,7 @@ const [requesterId, setRequesterId] = useState('');
   if (!data) return <div>Loading...</div>;
 
 //   new request
-  
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     setRequesterId(userId)
@@ -80,21 +114,24 @@ const [requesterId, setRequesterId] = useState('');
     } catch (error) {
       console.error('Error creating verification request:', error);
     }
+    setCIDModalOpen(false);
   };
 
-  // const handleCheckboxChange = (identity) => {
-  //   const index = details.indexOf(identity);
-  //   if (index === -1) {
-  //     setDetails([...details, identity]);
-  //   } else {
-  //     setDetails(details.filter(item => item !== identity));
-  //   }
-  // };
+  const handleSubmitOfContract = () => {
+    setContractModalOpen(false);
+    const indices = selectedIdentities.map(identity => fixedIdentities.indexOf(identity));
+
+    if (selectedIdentities.length > 0 && receiverId !== '') {
+      setContractResultModalOpen(true);
+      console.log('Receiver Address:', receiverId);
+      console.log('Selected Identities:', indices);
+    }
+  }
 
   return (
     <div>
         <h1>Create New Verification Request</h1>
-      <form onSubmit={handleSubmit}>
+      {/* <form onSubmit={handleSubmit}> */}
         <label>
           Receiver ID:
           <input
@@ -104,7 +141,35 @@ const [requesterId, setRequesterId] = useState('');
           />
         </label>
         <br />
-        <label>
+        <br />
+        <button onClick={handleDirectRequest}>Make Direct Request</button>
+        <button onClick={handleAskCID}>Ask for CIDs</button>
+        
+        <Modal isOpen={contractModalOpen}>
+        <h2>Choose Identities for Direct Request from smart contract</h2>
+        <MultiSelectDropdown
+          options={fixedIdentities}
+          selectedValues={selectedIdentities}
+          onChange={setSelectedIdentities}
+        />
+        <button onClick={handleSubmitOfContract}>Submit Request</button>
+        <button onClick={() => setContractModalOpen(false)}>Cancel</button>
+      </Modal>
+      { contractResultModalOpen && <ContractDataModal isOpen={contractResultModalOpen} onRequestClose={() => setContractResultModalOpen(false)} userAddress={receiverId} indices={selectedIdentities.map(identity => fixedIdentities.indexOf(identity))} /> }
+      
+      {/* Ask CID Modal */}
+      <Modal isOpen={CIDmodalOpen}>
+        <h2>Choose Identities to Ask for CIDs</h2>
+        <MultiSelectDropdown
+          options={fixedIdentities}
+          selectedValues={selectedIdentities}
+          onChange={setSelectedIdentities}
+        />
+        <button onClick={handleSubmit}>Submit Request</button>
+        <button onClick={() => setCIDModalOpen(false)}>Cancel</button>
+      </Modal>
+
+        {/* <label>
         Select Identities:
         <MultiSelectDropdown
             options={fixedIdentities}
@@ -112,10 +177,9 @@ const [requesterId, setRequesterId] = useState('');
             onChange={setSelectedIdentities}
           />
         </label>
-        <br />
-        <button type="submit">Submit Request</button>
-      </form>
-      <h1>Verification Requests made by User {userId}</h1>
+        <button type="submit">Submit Request</button> */}
+      {/* </form> */}
+      {/* <h1>Verification Requests made by User {userId}</h1>
       <ul>
         {filteredRequests.map((request) => (
           <li key={request._id}>
@@ -124,12 +188,11 @@ const [requesterId, setRequesterId] = useState('');
             <p>Details: {request.details.map(index => fixedIdentities[index]).join(', ')}</p>
             <p>Requester ID: {request.requesterId}</p>
             <p>Receiver ID: {request.receiverId}</p>
-            {/* Add other properties as needed */}
           </li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
 };
 
-export default YourComponent;
+export default VerifyDataPage;
