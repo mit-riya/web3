ecb7ec6ac12f42b66a4b43b0e170d8c5486dc132
Tanisha <tanishasalvi12@gmail.com>
2024-02-10 02:43:24 +0530

    added userContext

--- components/VerificationStatus.js
+++ components/VerificationStatus.js
@@ -2,24 +2,25 @@ import { useEffect, useState } from 'react';
 import Modal from 'react-modal';
 import web3 from '../contracts/web3';
 import styles from './../styles/modal.module.css';
+import { useContext } from 'react';
+import { UserContext } from './../pages/context/userContext';
 
 const ContractDataModal = ({ isOpen, onRequestClose, userAddress, indices }) => {
+    const { AllIdentities } = useContext(UserContext);
+    const { account } = useContext(UserContext);
     const [identityData, setIdentityData] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const contract = new web3.eth.Contract(process.env.CONTRACT_ABI, process.env.CONTRACT_ADDRESS);
-                const myAddress = (await web3.eth.getAccounts())[0];
-                const AllIdentities = await contract.methods.getAllIdentities().call({ from: myAddress });
-
                 console.log('userAddress:', userAddress);
                 console.log('indices:', indices);
 
                 const data = [];
                 for (let i = 0; i < indices.length; i++) {
                     const index = indices[i];
-                    const verificationStatus = await contract.methods.verifyIdentity(userAddress, index).call({ from: myAddress });
+                    const verificationStatus = await contract.methods.verifyIdentity(userAddress, index).call({ from: account });
                     data.push({ identity: AllIdentities[index], verificationStatus });
                 }
 
