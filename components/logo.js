fdc66dbc4d23be138fe815c735abc99e86b95c58
Himanshi <ghimanshi941@gmail.com>
2024-02-10 22:36:31 +0530

    changed logo size

--- components/logo.js
+++ components/logo.js
@@ -1,7 +1,7 @@
 import logo from "./../public/trustfolio1.svg"
 const Logo =()=>{
     return (
-        < div style={{width:'100px', height:'100px', position:'absolute', top:'0', right:'0', margin:'12px'}}>
+        < div style={{width:'60px', height:'60px', position:'absolute', top:'0', right:'0', margin:'12px'}}>
             <img src={logo.src} />
         </div>
     )
