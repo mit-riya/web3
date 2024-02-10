7df42cac961c566fa8999c961b953d7278f1f622
Tanisha <tanishasalvi12@gmail.com>
2024-02-10 14:53:27 +0530

    commented myIdentities

--- pages/myIdentities.js
+++ pages/myIdentities.js
@@ -1,3 +1,6 @@
+/* myIdentities.js - Page for managing user's identities */
+
+// Import libraries
 import { useEffect, useState } from 'react';
 import { useContext } from 'react';
 import web3 from '../contracts/web3';
@@ -6,95 +9,100 @@ import styles from './../styles/myIdentities.module.css';
 import { UserContext } from './context/userContext';
 import Navbar from '@/components/navbar';
 
+// Define the MyIdentities component
 const MyIdentities = () => {
-  const { account, loadingAccount } = useContext(UserContext);
-  const { AllIdentities } = useContext(UserContext);
-  const [identities, setIdentities] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [isUploaderOpen, setIsUploaderOpen] = useState(false);
-  const [verificationStatus, setVerificationStatus] = useState([]);
-  const [selectedIdentityIndex, setSelectedIdentityIndex] = useState(null);
-  const [addFunction, setAddFunction] = useState(true);
+  const { account, loadingAccount } = useContext(UserContext);  // Get the account and loadingAccount from the UserContext
+  const { AllIdentities } = useContext(UserContext); // Get the AllIdentities from the UserContext
+  const [identities, setIdentities] = useState([]); // Initialize the identities state variable
+  const [loading, setLoading] = useState(false); // Initialize the loading state variable
+  const [isUploaderOpen, setIsUploaderOpen] = useState(false);  // Initialize the isUploaderOpen state variable
+  const [verificationStatus, setVerificationStatus] = useState([]); // Initialize the verificationStatus state variable
+  const [selectedIdentityIndex, setSelectedIdentityIndex] = useState(null); // Initialize the selectedIdentityIndex state variable
+  const [addFunction, setAddFunction] = useState(true); // Initialize the addFunction state variable
+  const [expandedCategory, setExpandedCategory] = useState(null); // Toggle visibility of identities under each category
 
-  // useEffect(() => {
-  //   loadIdentities();
-  // }, []);
-
+  // Load the user's identities when the component mounts
   useEffect(() => {
     loadIdentities();
   }, [loadingAccount]);
 
+  // Function to load the user's identities
   const loadIdentities = async () => {
-    setLoading(true);
+    setLoading(true); // Set loading to true
 
     try {
       // Connect to the user's MetaMask provider
       if (window.ethereum) {
-        console.log('account : ', account);
-        console.log('All Identities : ', AllIdentities);
 
         // Load the DigitalIdentity contract using the ABI and contract address
         const contract = new web3.eth.Contract(process.env.CONTRACT_ABI, process.env.CONTRACT_ADDRESS);
-        const VerificationStatus = await contract.methods.getVerificationStatus().call({ from: account });
-        console.log(VerificationStatus);
+        const VerificationStatus = await contract.methods.getVerificationStatus().call({ from: account });  // Get the verification status of the user's identities
 
         // Update the state with the fetched identities
-        setIdentities(AllIdentities || []);
-        setVerificationStatus(VerificationStatus || []);
+        setIdentities(AllIdentities || []); // Set the identities state variable to the fetched identities
+        setVerificationStatus(VerificationStatus || []);  // Set the verificationStatus state variable to the fetched verification status
       } else {
+        // Log an error if MetaMask is not installed
         console.error('MetaMask is not installed');
       }
     } catch (error) {
+      // Log an error if there is an error loading the identities
       console.error('Error loading identities:', error.message);
     } finally {
+      // Set loading to false
       setLoading(false);
     }
   };
 
+  // Function to open the file uploader
   const openFileUploader = (index, functionCalled) => {
-    // setGeneratedCID(""); // Clear previous CID
+    // Set addFunction to true if the functionCalled is 0, otherwise set it to false
     if (functionCalled === 0) {
-      setAddFunction(false);
+      setAddFunction(false);  // Set addFunction to false
     } else {
-      setAddFunction(true);
+      setAddFunction(true); // Set addFunction to true
     }
-    setIsUploaderOpen(true);
-    setSelectedIdentityIndex(index);
+    setIsUploaderOpen(true);  // Set isUploaderOpen to true
+    setSelectedIdentityIndex(index);  // Set the selectedIdentityIndex to the index of document
   };
 
+  // Function to close the file uploader
   const closeFileUploader = () => {
-    setIsUploaderOpen(false);
-    setSelectedIdentityIndex(null);
-    // Reload identities after the FileUploader is closed
-    loadIdentities();
+    setIsUploaderOpen(false); // Set isUploaderOpen to false
+    setSelectedIdentityIndex(null); // Set the selectedIdentityIndex to null
+    loadIdentities(); // Reload identities after the FileUploader is closed
   };
 
+  // Function to handle updating an identity
   const handleUpdate = async (index, cid) => {
-    console.log("cid: ", cid);
-    console.log("identity index: ", selectedIdentityIndex);
+    // Check if the CID is generated and the selectedIdentityIndex is not null
     if (cid && selectedIdentityIndex !== null) {
+
       try {
         const contract = new web3.eth.Contract(
           process.env.CONTRACT_ABI,
           process.env.CONTRACT_ADDRESS
         );
 
+        // Update the identity in contract
         await contract.methods.updateIdentity(index, cid, true).send({ from: account });
-        console.log(`Identity updated: ${index}`);
+        console.log(`Identity updated`);  // Log a message if the identity is updated
       } catch (error) {
-        console.error('Error updating identity:', error.message);
+        console.error('Error updating identity:', error.message); // Log an error if there is an error updating the identity
       }
-      loadIdentities();
+      loadIdentities(); // Reload identities after the identity is updated
     }
     else {
-      console.error('Error updating identity: CID not generated');
+      console.error('Error updating identity: CID not generated');  // Log an error if the CID is not generated
     }
   };
 
+  // Function to handle deleting an identity
   const handleDelete = async (index) => {
     // Display confirmation dialog before deleting
     const isConfirmedDelete = window.confirm("Are you sure you want to delete this identity?");
 
+    // Check if the user confirmed the deletion
     if (isConfirmedDelete) {
       try {
         const contract = new web3.eth.Contract(
@@ -104,19 +112,19 @@ const MyIdentities = () => {
 
         // Delete the identity if confirmed
         await contract.methods.deleteIdentity(index).send({ from: account });
-        console.log(`Identity deleted: ${index}`);
+        console.log(`Identity deleted`);  // Log a message if the identity is deleted
         loadIdentities();
       } catch (error) {
-        console.error('Error deleting identity:', error.message);
+        console.error('Error deleting identity:', error.message); // Log an error if there is an error deleting the identity
       }
     } else {
-      console.log("Deletion canceled by user.");
+      console.log("Deletion canceled by user.");  // Log a message if the deletion is canceled by the user
     }
   };
 
+  // Function to handle adding an identity
   const handleAdd = async (index, cid) => {
-    console.log("cid: ", cid);
-    console.log("identity index: ", selectedIdentityIndex);
+    // Check if the CID is generated and the selectedIdentityIndex is not null
     if (cid && selectedIdentityIndex !== null) {
       try {
         const contract = new web3.eth.Contract(
@@ -124,15 +132,15 @@ const MyIdentities = () => {
           process.env.CONTRACT_ADDRESS
         );
 
-        console.log('index : ', index);
+        // Add the identity to the contract
         await contract.methods.addIdentity(index, cid, true).send({ from: account });
-        console.log(`Identity added: ${index}`);
-        loadIdentities();
+        console.log(`Identity added`);  // Log a message if the identity is added
+        loadIdentities(); // Reload identities after the identity is added
       } catch (error) {
-        console.error('Error adding identity:', error.message);
+        console.error('Error adding identity:', error.message); // Log an error if there is an error adding the identity
       }
     } else {
-      console.error('Error adding identity: CID not generated');
+      console.error('Error adding identity: CID not generated');  // Log an error if the CID is not generated
     }
   };
 
@@ -140,37 +148,26 @@ const MyIdentities = () => {
   const groupedIdentities = identities.reduce((groups, identity) => {
     const category = identity.split(' - ')[0]; // identities are categorized with ' - '
     if (!groups[category]) {
-      groups[category] = [];
+      groups[category] = [];  // Create a new category if it doesn't exist
     }
-    groups[category].push(identity);
-    return groups;
+    groups[category].push(identity);  // Add the identity to the category
+    return groups;  // Return the updated groups
   }, {});
 
-  // Toggle visibility of identities under each category
-  const [expandedCategory, setExpandedCategory] = useState(null);
-
-  const [originalIdentities, setOriginalIdentities] = useState([]);
-
-  useEffect(() => {
-    // Populate original identities list when loading identities
-    setOriginalIdentities(identities);
-  }, [identities]);
-
-  const getOriginalIndex = (identity, index) => {
+  // Function to get the original index of the identity
+  const getOriginalIndex = (identity) => {
     // Find the original index of the identity based on the current index
-    console.log(identity);
-    console.log(index);
-    console.log(identities.findIndex((x) => x === identity));
-
     return identities.findIndex((_identity) => _identity === identity);
   };
 
+  // Function to handle the click event
   async function handleClick(index, isVerified) {
     try {
       const contract = new web3.eth.Contract(
         process.env.CONTRACT_ABI,
         process.env.CONTRACT_ADDRESS
       );
+      // Open the file in a new tab if verified
       if (isVerified) {
         const cid = await contract.methods.getCID(index).call({ from: account });
         const target_url = `${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/${cid}?pinataGatewayToken=${process.env.PINATA_URL_SECOND}`;
@@ -179,19 +176,23 @@ const MyIdentities = () => {
         // no action
       }
     } catch (error) {
-      console.error('Error:', error.message);
+      console.error('Error:', error.message); // Log an error if there is an error
     }
   }
 
-
+  // Return the MyIdentities component
   return (
     <div className={styles.container}>
       <Navbar/>
-      <h1 className={styles.heading}>My Info</h1>
-      {loading && <p>Loading...</p>}
-      {!account && <p>No identities found.</p>}
+      <h1 className={styles.heading}>My Info</h1> {/* Heading for the page */}
+
+      {loading && <p>Loading...</p>}  {/* Display a loading message if the identities are loading */}
+      {!account && <p>No identities found.</p>} {/* Display a message if no identities are found */}
+
+      {/* Display the identities if they are found */}
       {account && !loading && Object.keys(groupedIdentities).length > 0 && (
         <div className={styles.tileWrapper}>
+          {/* Map through the grouped identities and display them */}
           {Object.entries(groupedIdentities).map(([category, identities]) => (
             <div key={category} className={styles.tileWrapper}>
               <h2 className={styles.categoryHeader} onClick={() => setExpandedCategory(expandedCategory === category ? null : category)}>
@@ -201,15 +202,15 @@ const MyIdentities = () => {
                 <div>
                   {identities.map((identity, index) => (
                     <div key={index} className={styles.identitiesList}>
-                      <p className={styles.aligntext} onClick={() => handleClick(getOriginalIndex(identity, index), verificationStatus[getOriginalIndex(identity, index)])}>{identity.includes(' - ') ? identity.split(' - ')[1] : identity}</p>
+                      <p className={styles.aligntext} onClick={() => handleClick(getOriginalIndex(identity), verificationStatus[getOriginalIndex(identity)])}>{identity.includes(' - ') ? identity.split(' - ')[1] : identity}</p>
                       <div className={styles.buttonGroup}>
-                        {verificationStatus[getOriginalIndex(identity, index)] ? (
+                        {verificationStatus[getOriginalIndex(identity)] ? (
                           <>
-                            <button className={styles.buttonAdd} disabled={isUploaderOpen} onClick={() => openFileUploader(getOriginalIndex(identity, index), 0)}>Update</button>
-                            <button className={styles.buttonAdd} disabled={isUploaderOpen} onClick={() => handleDelete(getOriginalIndex(identity, index))}>Delete</button>
+                            <button className={styles.buttonAdd} disabled={isUploaderOpen} onClick={() => openFileUploader(getOriginalIndex(identity), 0)}>Update</button>
+                            <button className={styles.buttonAdd} disabled={isUploaderOpen} onClick={() => handleDelete(getOriginalIndex(identity))}>Delete</button>
                           </>
                         ) : (
-                          <button className={styles.buttonAdd} disabled={isUploaderOpen} onClick={() => openFileUploader(getOriginalIndex(identity, index), 1)}>Add</button>
+                          <button className={styles.buttonAdd} disabled={isUploaderOpen} onClick={() => openFileUploader(getOriginalIndex(identity), 1)}>Add</button>
                         )}
                       </div>
                     </div>
@@ -220,6 +221,7 @@ const MyIdentities = () => {
           ))}
         </div>
       )}
+      {/* Display the file uploader if the user is logged in and the identities are found */}
       {isUploaderOpen && (
         <div style={{ position: 'absolute', top: '0px', width: 'fit-content', height: 'fit-content', backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
           <FileUploader
