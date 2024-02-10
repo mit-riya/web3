c33e599ffcd7aa5bf3cfd62997ac9d3c02859dda
Tanisha <tanishasalvi12@gmail.com>
2024-02-10 16:21:28 +0530

    commented index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
-import { useState, useRef } from "react";
-// pages/index.js
+/* index.js */
+
 import Head from "next/head";
-import Chatbot from "./chat";
-import Navbar from "@/components/navbar";
 import Login from "./login";
 
 const Home = () => {
@@ -10,7 +8,6 @@ const Home = () => {
     <>
       <Head>
         <title>Your Next.js App</title>
-        {/* Add other head elements if needed */}
       </Head>
       <div>
         <Login/>
